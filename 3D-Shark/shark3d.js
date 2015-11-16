20b15483da2270a14029b47725641614ca7a549a
kmacdon1 <kmacdon1@lakeheadu.ca>
2015-11-16 17:01:11 -0500

    removed useless camera variables
    turn view with left/right arrows

--- 3D-Shark/shark3d.js
+++ 3D-Shark/shark3d.js
@@ -42,7 +42,6 @@ var materialShininess = 100.0;
 var ctm;
 var ambientColor, diffuseColor, specularColor;
 var modelView, projection;
-var viewerPos;
 var program;
 
 var xAxis = 0;
@@ -51,22 +50,7 @@ var zAxis = 2;
 var axis = 0;
 var theta =[0, 0, 0];
 
-var thetaLoc;
-
-var near = 0.3;
-var far = 3.0;
-var radius = 4.0;
-var vtheta  = 0.0;
-var phi    = 0.0;
-var dr = 5.0 * Math.PI/180.0;
-var fovy = 45.0;  // Field-of-view in Y direction angle (in degrees)
-var aspect = 1.0;       // Viewport aspect ratio
-
-var modelViewMatrix, projectionMatrix;
 var modelViewMatrixLoc, projectionMatrixLoc;
-var eye;
-const at = vec3(0.0, 0.0, 0.0);
-const up = vec3(0.0, 1.0, 0.0);
 
 var turnLeft = false;
 var turnRight = false;
@@ -95,8 +79,7 @@ function quad(a, b, c, d) {
      normalsArray.push(normal);    
 }
 
-function colorCube()
-{
+function colorCube(){
     quad( 1, 0, 3, 2 );
     quad( 2, 3, 7, 6 );
     quad( 3, 0, 4, 7 );
@@ -120,9 +103,7 @@ window.onload = function init() {
 	//event listeners
 	document.onkeyup = handleKeyUp;
 	
-    //
-    //  Load shaders and initialize attribute buffers
-    //
+    // Load shaders and initialize attribute buffers
     program = initShaders( gl, "vertex-shader", "fragment-shader" );
     gl.useProgram( program );
     
@@ -147,10 +128,6 @@ window.onload = function init() {
 	modelViewMatrixLoc = gl.getUniformLocation( program, "modelViewMatrix" );
     projectionMatrixLoc = gl.getUniformLocation( program, "projectionMatrix" );
 
-    thetaLoc = gl.getUniformLocation(program, "theta"); 
-    
-    viewerPos = vec3(0.0, 0.0, -20.0 );
-
     projection = ortho(-1, 1, -1, 1, -100, 100);
     
     ambientProduct = mult(lightAmbient, materialAmbient);
@@ -168,21 +145,10 @@ window.onload = function init() {
     gl.uniform4fv(gl.getUniformLocation(program, "lightPosition"), 
        flatten(lightPosition) );
        
-    gl.uniform1f(gl.getUniformLocation(program, 
-       "shininess"),materialShininess);
+    gl.uniform1f(gl.getUniformLocation(program, "shininess"),materialShininess);
     
-    gl.uniformMatrix4fv( gl.getUniformLocation(program, "projectionMatrix"),
-       false, flatten(projection));
+    gl.uniformMatrix4fv( projectionMatrixLoc, false, flatten(projection));
     
-	// viewing parameters (eye must be at origin)
-	far = 3; //min 3 max 10 step 3.0 
-	near = 0.3; //min .01 max 3 step 0.1 < refine this to inside cube >
-	radius = 4; // min 0.05 max 10 step 0.1
-	vtheta = 0; // min -90 max 90 step 5
-	phi = 0; // min -90 max 90 step 5
-	aspect = 1; // min 0.5 max 2 step 0.1
-	fovy = 45; // min 10 max 120 step 5
-	
     render();
 }
 
@@ -230,9 +196,9 @@ function rotateView(){
 }
 
 var render = function(){
-            
+	
     gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-            
+	
 	rotateView();
 	
     modelView = mat4();
@@ -240,16 +206,9 @@ var render = function(){
     modelView = mult(modelView, rotate(theta[yAxis], [0, 1, 0] ));
     modelView = mult(modelView, rotate(theta[zAxis], [0, 0, 1] ));
     
-    gl.uniformMatrix4fv( gl.getUniformLocation(program, "modelViewMatrix"), false, flatten(modelView) );
-	//gl.uniformMatrix4fv( modelViewMatrixLoc, false, flatten(modelViewMatrix) );
-    
-	eye = vec3(radius*Math.sin(vtheta)*Math.cos(phi), radius*Math.sin(vtheta)*Math.sin(phi), radius*Math.cos(vtheta));
-    modelViewMatrix = lookAt(eye, at , up);
-    projectionMatrix = perspective(fovy, aspect, near, far);
-
-    //gl.uniformMatrix4fv( projectionMatrixLoc, false, flatten(projectionMatrix) );
-			
+    gl.uniformMatrix4fv( modelViewMatrixLoc, false, flatten(modelView) );
+	
     gl.drawArrays( gl.TRIANGLES, 0, numVertices );
-            
+	
     requestAnimFrame(render);
 }
