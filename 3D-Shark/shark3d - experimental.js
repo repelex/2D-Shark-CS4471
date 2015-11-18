b36f5c4a4abdfa1174798315e674df8583c6b25f
repelex <repelex_1@hotmail.com>
2015-11-18 16:18:15 -0500

    made some 3d garbage
    literal trash

---
+++ 3D-Shark/shark3d - experimental.js
@@ -0,0 +1,320 @@
+var canvas;
+var gl;
+
+var pointsArray = [];
+var normalsArray = [];
+var colorsArray = [];
+var texCoordsArray = [];
+
+var texture;
+
+var texCoord = [
+    vec2(0, 0),
+    vec2(0, 1),
+    vec2(1, 1),
+    vec2(1, 0)
+];
+
+var vertices = [
+    vec4( -0.5, -0.5,  0.5, 1.0 ),
+    vec4( -0.5,  0.5,  0.5, 1.0 ),
+    vec4( 0.5,  0.5,  0.5, 1.0 ),
+    vec4( 0.5, -0.5,  0.5, 1.0 ),
+    vec4( -0.5, -0.5, -0.5, 1.0 ),
+    vec4( -0.5,  0.5, -0.5, 1.0 ),
+    vec4( 0.5,  0.5, -0.5, 1.0 ),
+    vec4( 0.5, -0.5, -0.5, 1.0 )
+];
+
+var vertexColors = [
+    vec4( 0.0, 0.0, 0.0, 1.0 ),  // black
+    vec4( 1.0, 0.0, 0.0, 1.0 ),  // red
+    vec4( 1.0, 1.0, 0.0, 1.0 ),  // yellow
+    vec4( 0.0, 1.0, 0.0, 1.0 ),  // green
+    vec4( 0.0, 0.0, 1.0, 1.0 ),  // blue
+    vec4( 1.0, 0.0, 1.0, 1.0 ),  // magenta
+    vec4( 0.0, 1.0, 1.0, 1.0 ),  // white
+    vec4( 0.0, 1.0, 1.0, 1.0 )   // cyan
+];
+
+
+// shark cage walls
+var cage_south = [
+        vec4( -0.5, -0.5,  0.5, 1.0 ),
+        vec4( -0.5,  0.5,  0.5, 1.0 ),
+        vec4( 0.5,  0.5,  0.5, 1.0 ),
+        vec4( 0.5, -0.5,  0.5, 1.0 )
+	];
+var cage_north = [
+		vec4( -0.5, -0.5, -0.5, 1.0 ),
+        vec4( -0.5,  0.5, -0.5, 1.0 ),
+        vec4( 0.5,  0.5, -0.5, 1.0 ),
+        vec4( 0.5, -0.5, -0.5, 1.0 )
+	];
+var cage_east = [
+		vec4( 0.5, -0.5, -0.5, 1.0 ),
+        vec4( 0.5,  0.5, -0.5, 1.0 ),
+        vec4( 0.5,  0.5, 0.5, 1.0 ),
+        vec4( 0.5, -0.5, 0.5, 1.0 )
+	];
+var cage_west = [
+		vec4( -0.5, -0.5, 0.5, 1.0 ),
+        vec4( -0.5,  0.5, 0.5, 1.0 ),
+        vec4( -0.5,  0.5, -0.5, 1.0 ),
+        vec4( -0.5, -0.5, -0.5, 1.0 )
+	];
+var cage_top = [
+		vec4( -0.5, 0.5, -0.5, 1.0 ),
+        vec4( -0.5,  0.5, 0.5, 1.0 ),
+        vec4( 0.5,  0.5, 0.5, 1.0 ),
+        vec4( 0.5, 0.5, -0.5, 1.0 )
+	];
+var cage_bottom = [
+		vec4( -0.5, -0.5, -0.5, 1.0 ),
+        vec4( -0.5, -0.5, 0.5, 1.0 ),
+        vec4( 0.5,  -0.5, 0.5, 1.0 ),
+        vec4( 0.5, -0.5, -0.5, 1.0 )
+	];
+
+//color reference   
+/* var vertexColors = [
+    vec4( 0.0, 0.0, 0.0, 1.0 ),  // black
+    vec4( 1.0, 0.0, 0.0, 1.0 ),  // red
+    vec4( 1.0, 1.0, 0.0, 1.0 ),  // yellow
+    vec4( 0.0, 1.0, 0.0, 1.0 ),  // green
+    vec4( 0.0, 0.0, 1.0, 1.0 ),  // blue
+    vec4( 1.0, 0.0, 1.0, 1.0 ),  // magenta
+    vec4( 0.0, 1.0, 1.0, 1.0 ),  // cyan
+    vec4( 1.0, 1.0, 1.0, 1.0 ),  // white
+]; */
+
+var lightPosition = vec4(0.0, 1.0, 0.0, 0.0 );
+var lightAmbient = vec4(0.2, 0.2, 0.2, 1.0 );
+var lightDiffuse = vec4( 1.0, 1.0, 1.0, 1.0 );
+var lightSpecular = vec4( 1.0, 0.0, 0.0, 1.0 );
+
+var materialAmbient = vec4( 1.0, 1.0, 1.0, 1.0 );
+var materialDiffuse = vec4( 1.0, 1.0, 1.0, 1.0);
+var materialSpecular = vec4( 1.0, 1.0, 1.0, 1.0 );
+var materialShininess = 100.0;
+
+var ctm;
+var ambientColor, diffuseColor, specularColor;
+var modelView, projection;
+var program;
+
+var xAxis = 0;
+var yAxis = 1;
+var zAxis = 2;
+var axis = yAxis;
+var theta =[0, 0, 0];
+
+var modelViewMatrixLoc, projectionMatrixLoc;
+
+var turnLeft = false;
+var turnRight = false;
+var turning = false;
+var turnRate = 2.0;
+var degToTurn;
+
+const eye = vec3(0.0, 0.0, 0.0);
+const at = vec3(1.0, 0.0, 0.0);
+const up = vec3(0.0, 1.0, 0.0);
+var near = 0.3;
+var far = 3.0;
+var fov = 110.0;  // Field-of-view in Y direction angle (in degrees)
+var aspect;
+
+function configureTexture( image ) {
+    texture = gl.createTexture();
+    gl.bindTexture( gl.TEXTURE_2D, texture );
+    gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
+    gl.texImage2D( gl.TEXTURE_2D, 0, gl.RGB,
+         gl.RGB, gl.UNSIGNED_BYTE, image );
+    gl.generateMipmap( gl.TEXTURE_2D );
+    gl.texParameteri( gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER,
+                      gl.NEAREST_MIPMAP_LINEAR );
+    gl.texParameteri( gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST );
+
+
+    gl.uniform1i(gl.getUniformLocation(program, "texture"), 0);
+}
+
+
+function quad(a, b, c, d) {
+     pointsArray.push(vertices[a]);
+     colorsArray.push(vertexColors[a]);
+     texCoordsArray.push(texCoord[0]);
+
+     pointsArray.push(vertices[b]);
+     colorsArray.push(vertexColors[a]);
+     texCoordsArray.push(texCoord[1]);
+
+     pointsArray.push(vertices[c]);
+     colorsArray.push(vertexColors[a]);
+     texCoordsArray.push(texCoord[2]);
+
+     pointsArray.push(vertices[a]);
+     colorsArray.push(vertexColors[a]);
+     texCoordsArray.push(texCoord[0]);
+
+     pointsArray.push(vertices[c]);
+     colorsArray.push(vertexColors[a]);
+     texCoordsArray.push(texCoord[2]);
+
+     pointsArray.push(vertices[d]);
+     colorsArray.push(vertexColors[a]);
+     texCoordsArray.push(texCoord[3]);
+}
+
+
+function colorCage()
+{
+    quad( 5, 4, 0, 1 );
+    quad( 1, 0, 3, 2 );
+    quad( 2, 3, 7, 6 );
+    quad( 3, 0, 4, 7 );
+    quad( 6, 5, 1, 2 );
+    quad( 4, 5, 6, 7 );
+    
+}
+
+window.onload = function init() {
+    
+    canvas = document.getElementById( "gl-canvas" );
+    
+    // configure webgl
+    gl = WebGLUtils.setupWebGL( canvas );
+    if ( !gl ) { alert( "WebGL isn't available" ); }
+    gl.viewport( 0, 0, canvas.width, canvas.height );
+    aspect =  canvas.width/canvas.height;
+    gl.clearColor( 0.0, 0.0, 1.0, 1.0 );
+    gl.disable(gl.DEPTH_TEST);
+    gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA);
+    gl.enable(gl.BLEND);
+
+    //event listeners
+    document.onkeyup = handleKeyUp;
+    
+    // Load shaders and initialize attribute buffers
+    program = initShaders( gl, "vertex-shader-ex", "fragment-shader-ex" );
+    gl.useProgram( program );
+    
+    colorCage();
+   
+    var cBuffer = gl.createBuffer();
+    gl.bindBuffer( gl.ARRAY_BUFFER, cBuffer );
+    gl.bufferData( gl.ARRAY_BUFFER, flatten(colorsArray), gl.STATIC_DRAW );
+
+    var vColor = gl.getAttribLocation( program, "vColor" );
+    gl.vertexAttribPointer( vColor, 4, gl.FLOAT, false, 0, 0 );
+    gl.enableVertexAttribArray( vColor );
+
+    var vBuffer = gl.createBuffer();
+    gl.bindBuffer( gl.ARRAY_BUFFER, vBuffer );
+    gl.bufferData( gl.ARRAY_BUFFER, flatten(pointsArray), gl.STATIC_DRAW );
+
+    var vPosition = gl.getAttribLocation( program, "vPosition" );
+    gl.vertexAttribPointer( vPosition, 4, gl.FLOAT, false, 0, 0 );
+    gl.enableVertexAttribArray( vPosition );
+
+    var tBuffer = gl.createBuffer();
+    gl.bindBuffer( gl.ARRAY_BUFFER, tBuffer );
+    gl.bufferData( gl.ARRAY_BUFFER, flatten(texCoordsArray), gl.STATIC_DRAW );
+
+    var vTexCoord = gl.getAttribLocation( program, "vTexCoord" );
+    gl.vertexAttribPointer( vTexCoord, 2, gl.FLOAT, false, 0, 0 );
+    gl.enableVertexAttribArray( vTexCoord );
+
+    modelViewMatrixLoc = gl.getUniformLocation( program, "modelViewMatrix" );
+    projectionMatrixLoc = gl.getUniformLocation( program, "projectionMatrix" );
+
+    //
+    // Initialize a texture
+    //
+
+    //var image = new Image();
+    //image.onload = function() {
+     //   configureTexture( image );
+    //}
+    //image.src = "SA2011_black.gif"
+
+
+    var image = document.getElementById("texImage");
+
+    configureTexture( image );
+
+    thetaLoc = gl.getUniformLocation(program, "theta");
+
+    render();
+}
+
+function handleKeyUp(event) {
+    
+    if (event.keyCode == 37 || event.keyCode ==  65) {
+        // left arrow key or A
+        if (!turning){
+            turnRight = false;
+            turnLeft = true;
+            degToTurn = 90;
+            turning = true;
+        }
+    } else if (event.keyCode == 39 || event.keyCode == 68) {
+        // right arrow key or D
+        if (!turning){
+            turnLeft = false;
+            turnRight = true;
+            degToTurn = 90;
+            turning = true;
+        }
+
+    } else if (event.keyCode == 32) {
+        // spacebar
+        alert(theta[axis]);
+    } else if (event.keyCode == 13) {
+        // reload game
+        location.reload();
+    }
+}
+
+function rotateView(){
+    
+    if (turnLeft){
+        theta[axis] -= turnRate;
+        degToTurn -= turnRate;
+        if (degToTurn == 0) {
+            turnLeft=false;
+            turning = false;
+        }
+    }
+    if (turnRight){
+        theta[axis] += turnRate;
+        degToTurn -= turnRate;
+        if (degToTurn == 0){ 
+            turnRight=false;
+            turning = false;
+        }
+    }
+}
+
+var render = function(){
+    
+    gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+    rotateView();
+
+    modelView = lookAt(eye, at, up);
+    modelView = mult(modelView, rotate(theta[xAxis], [1, 0, 0] ));
+    modelView = mult(modelView, rotate(theta[yAxis], [0, 1, 0] ));
+    modelView = mult(modelView, rotate(theta[zAxis], [0, 0, 1] ));
+    
+    gl.uniformMatrix4fv( modelViewMatrixLoc, false, flatten(modelView) );
+ 
+    projection = perspective(fov, aspect, 0.1, 10);
+
+    gl.uniformMatrix4fv( projectionMatrixLoc, false, flatten(projection) );
+    
+    gl.drawArrays( gl.TRIANGLES, 0, pointsArray.length);
+    
+    requestAnimFrame(render);
+	
+}
