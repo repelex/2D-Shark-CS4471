48fa1f2e76bd343a973c181e0e15f7ac970f71d5
kmacdon1 <kmacdon1@lakeheadu.ca>
2015-11-18 16:35:29 -0500

    crappy shark face

--- 3D-Shark/shark3d.js
+++ 3D-Shark/shark3d.js
@@ -49,6 +49,28 @@ var shark = [
         vec4( 0.3, -0.3, -0.6, 1.0 )
 	];
 
+var sharkmouth = [
+		vec4( -0.1, -0.1, -0.6, 1.0 ),
+        vec4( -0.2,  0.2, -0.6, 1.0 ),
+        vec4( 0.2,  0.2, -0.6, 1.0 ),
+        vec4( 0.1, -0.1, -0.6, 1.0 )
+	];
+
+var sharkeye1 = [
+		vec4( -0.4, 0.4, -0.6, 1.0 ),
+        vec4( -0.35,  0.4, -0.6, 1.0 ),
+        vec4( -0.25,  0.4, -0.6, 1.0 ),
+        vec4( -0.35, 0.3, -0.6, 1.0 )
+	];
+	
+var sharkeye2 = [
+		vec4( 0.4, 0.4, -0.6, 1.0 ),
+        vec4( 0.35,  0.4, -0.6, 1.0 ),
+        vec4( 0.25,  0.4, -0.6, 1.0 ),
+        vec4( 0.35, 0.3, -0.6, 1.0 )
+	];
+
+
 //color reference   
 /* var vertexColors = [
     vec4( 0.0, 0.0, 0.0, 1.0 ),  // black
@@ -131,6 +153,9 @@ function colorCage(){
 
 function colorShark(){
 	quad(shark, 1, 0, 3, 2);
+	quad(sharkmouth, 0, 1, 2, 3);
+	quad(sharkeye1, 0, 1, 2, 3);
+	quad(sharkeye2, 0, 1, 2, 3);
 }
 
 window.onload = function init() {
