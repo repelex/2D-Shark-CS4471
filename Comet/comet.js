bc07b4c7a6fc413fd9f4eb5b2a53ce113fca5224
kmacdon1 <kmacdon1@lakeheadu.ca>
2015-11-27 23:07:08 -0500

    initial comet files

---
+++ Comet/comet.js
@@ -0,0 +1,19 @@
+/* 
+1. 	The planet is centered at the origin
+2. 	The planet is rotating (So some surface detail is needed)
+3. 	A moon is orbiting around the planet
+4. 	The comet movement on its trajectory (use a line, ignoring
+	the finer physics of orbits and gravity) is animated as it 
+	approaches and impacts the planet.
+5. 	Some graphic depiction of the impact is presented (for 
+	example a sphere that
+	diminishes to zero radius or a particle system explosion. 
+	This feature must rotate with the planet.)
+6.	Use Particle system methods for comet tail (pointing away 
+	from the sun)
+7.	Include lighting where the light comes from the sun 
+*/
+
+//useful rotating demo: http://learningwebgl.com/lessons/lesson13/index.html
+//maps for earth and moon: http://maps.jpl.nasa.gov/
+//useful particles demo: http://jeshua.me/content/demos/webglTutorial01/particleDemoLive.html
